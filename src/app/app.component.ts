1de76c0683d44b136154164efe59f3c9d2d6e444
Ten Li <li.tw.ten@gmail.com>
2023-01-10 09:34:25 +0800

    shell

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, QueryList, ViewChildren } from '@angular/core';
+import { MatButtonToggleChange } from '@angular/material/button-toggle';
+import { SplitAreaDirective } from 'angular-split';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  @ViewChildren(SplitAreaDirective)
+  splitAreas!: QueryList<SplitAreaDirective>;
+
   title = 'HMI_web_design';
+  leftSize = 20;
+  leftSizeStorage = this.leftSize;
+  rightSize = 20;
+  leftPanel = 'home';
+  isLeftPanelClose = false;
+
+  public homeClick(e: MatButtonToggleChange): void {
+    if (this.leftPanel !== e.value) {
+      this.leftPanel = e.value;
+      this.expandLeftPanel();
+    } else { 
+      this.leftPanel = e.value;
+      this.toggleLeftPanel();
+    }
+  }
+
+  private toggleLeftPanel(): void {
+    if (this.isLeftPanelClose) {
+      this.expandLeftPanel();
+    } else {
+      this.collapseLeftPanel();
+    }
+  }
+
+  private collapseLeftPanel(): void {
+    this.leftSizeStorage = this.leftSize;
+    this.leftSize = 0;
+    this.isLeftPanelClose = true;
+    this.splitAreas.first.collapse();
+  }
+
+  private expandLeftPanel(): void {
+    this.leftSize = this.leftSizeStorage;
+    this.isLeftPanelClose = false;
+    this.splitAreas.first.expand();
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { AngularSplitModule } from 'angular-split';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonToggleModule,
+    AngularSplitModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
